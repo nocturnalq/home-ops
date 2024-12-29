59c5198573117f1f085e053b39b6f5570acfb9cd
Leonid Filippov <l.filippov@1440.space>
2024-12-29 15:25:42 +0300

    scripts/k3s-install.sh: add color

--- scripts/k3s-install.sh
+++ scripts/k3s-install.sh
@@ -9,13 +9,19 @@ NC='\033[0m' # No Color
 echo -e "${GREEN}Starting K3s installation...${NC}"
 
 k3sup install \
-  --ip <SERVER_IP> \
-  --user <SSH_USER> \
-  --ssh-key <PATH_TO_SSH_KEY> \
-  --k3s-extra-args '--disable traefik --disable flannel'
+  --cluster \
+  --host $HOST \
+  --user $USER \
+  --ssh-key $KEY \
+  --k3s-extra-args '--disable traefik --disable flannel --disable servicelb' \
+  --merge \
+  --local-path $HOME/.kube/config \
+  --context home-ops-k3s \
+  --k3s-version v1.31.4+k3s1
 
 if [ $? -eq 0 ]; then
   echo -e "${GREEN}K3s installation completed successfully!${NC}"
 else
   echo -e "${RED}K3s installation failed.${NC}"
 fi
+
